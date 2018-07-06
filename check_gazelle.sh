1edd45a372de28b75071082b9687f4c42875b092
Preston Van Loon <preston@machinepowered.com>
2018-07-06 18:20:28 -0400

    add check gazelle script and fix issues in WORKSPACE

---
+++ check_gazelle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Continous Integration script to check that BUILD.bazel files are as expected
+# when generated from gazelle.
+
+# Duplicate stdout with redirection to 5.
+exec 5>&1
+
+# Run gazelle while piping a copy of the output to stdout via 5.
+changes=$(bazel run //:gazelle -- fix --mode=diff | tee >(cat - >&5))
+
+# If the captured stdout is not empty then Gazelle has diffs.
+if [ -z "$changes" ]
+then
+  echo "OK: Gazelle does not need to be run"
+  exit 0
+else
+  echo "FAIL: Gazelle needs to be run"
+  echo "Please run bazel run //:gazelle -- fix"
+  exit 1
+fi
